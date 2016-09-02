620eadcbaa6ac8a02d8a0e0c3114956aa4327852
NSAPS <nuy@nongshim.co.kr>
2016-09-02 10:09:17 +0900

    license 폴더는 삭제

--- license/license.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Copyright (c) 2001-2004 Zionex Corporation. All rights reserved.
-echo license seed generator
-
-set LOCALCLASSPATH=./;./license.jar
-
-java -cp "%LOCALCLASSPATH%" com.zionex.license.LicenseSeedGen
-
